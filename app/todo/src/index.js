8c3fd5458beef29e12c6d4d45cf9c98f3d98de2a
vvuux <phamngcvu21@gmail.com>
2022-06-09 15:47:48 +0700

    edit todo item

---
+++ app/todo/src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+// component files
+import TodoContainer from "./components/TodoContainer";
+
+// stylesheet
+import "./App.css"
+
+ReactDOM.render(
+  <React.StrictMode>
+    <TodoContainer/>
+  </React.StrictMode>, 
+    document.getElementById("root")
+)
